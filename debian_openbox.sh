e086ce9079291d4d0f3eacb0a3af2c649f74803d
Marco Giordano <marco@jessie.portasoprana>
2016-11-19 17:15:13 +0100

    ".config" folder added

--- debian_openbox.sh
+++ debian_openbox.sh
@@ -9,25 +9,29 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo Upgrading sistem
-apt update && apt upgrade
+apt-get update && apt-get -y upgrade
 
 echo Sudo installation & configuration
-aptitude install sudo
+apt-get -y install sudo
 gpasswd -a $MYUSER sudo
 
 echo Installing bare minimum
-apt install ntp vim screen htop
+apt-get -y install ntp vim screen htop
 
 echo Installing OpenBox and Slim
-# aptitude install xorg xinit openbox openbox-menu obconf obmenu lxappearance tint2 nitrogen pcmanfm gmrun slim
+apt-get -y install xorg xinit openbox openbox-menu obconf obmenu lxappearance tint2 nitrogen pcmanfm gmrun slim
 
 echo Installing goodies
-apt install vim terminator geany firefox-esr htop screen
-
-echo Checking out Openbox configuration files
-echo TODO
+apt-get -y install vim terminator geany firefox-esr htop screen
 
 echo Get ready for VBox additions
-apt-get install build-essential linux-headers-amd64
+apt-get -y install build-essential linux-headers-amd64
 
+echo Checking out Openbox configuration files
+cp -r config/.config/openbox /home/${MYUSER}/.config/
+cp -r config/.config/gtk-3.0 /home/${MYUSER}/.config/
+
+
+echo Fix permission
+chown -R $MYUSER:$MYUSER /home/${MYUSER}/.config
 
